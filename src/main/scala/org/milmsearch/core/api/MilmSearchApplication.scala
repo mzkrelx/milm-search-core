626ef0ab5fe23e810a7b6dca705d9a4d137915ca
Mizuki Yamanaka <charles.vineyard@gmail.com>
2012-08-22 21:04:29 +0900

    HelloWarld作成
    /api にアクセスすると Hello, World と表示されるサンプルを作成。
    リソースクラスの作成と、Application クラスの getClasses で
    作ったリソースクラスを返すと動く。

---
+++ src/main/scala/org/milmsearch/core/api/MilmSearchApplication.scala
@@ -0,0 +1,12 @@
+package org.milmsearch.core.api
+
+import javax.ws.rs.core.Application
+import scala.collection.JavaConverters._
+
+class MilmSearchApiApplication extends Application {
+  override def getClasses(): java.util.Set[Class[_]] = {
+    Set[Class[_]](
+        classOf[HelloResource]
+    ).asJava
+  }
+}
