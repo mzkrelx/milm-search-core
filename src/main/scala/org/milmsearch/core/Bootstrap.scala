d2151f2cc0b1968505026c237ca778eb334359dd
Yamanaka Yuu <yuu@relx.jp>
2012-10-07 22:57:14 +0900

    実行モードによってDB接続先を切り替える仕組みを作成
    ・src/main/resources/props 配下の properties ファイルに
      設定値を書けば良い。
    
    production.default.props ← 本番環境で読まれる
    default.props            ← 開発環境で読まれる
    test.default.props       ← テスト実行時に読まれる
    
    refs #31

--- src/main/scala/org/milmsearch/core/Bootstrap.scala
+++ src/main/scala/org/milmsearch/core/Bootstrap.scala
@@ -1,26 +1,61 @@
 package org.milmsearch.core
-import javax.servlet.ServletContextListener
+
+import org.milmsearch.common.LoggingUtil.withErrlog
+import org.milmsearch.common.LoggingUtil.withErrlogQuietly
+
 import javax.servlet.ServletContextEvent
-import net.liftweb.db.StandardDBVendor
-import net.liftweb.http.LiftRules
-import net.liftweb.mapper.Schemifier
+import javax.servlet.ServletContextListener
+
+import net.liftweb.common.Box.box2Option
+import net.liftweb.common.Loggable
+import net.liftweb.db.DB1.db1ToDb
+import net.liftweb.db.ProtoDBVendor
+import net.liftweb.mapper.DefaultConnectionIdentifier
+import net.liftweb.mapper.StandardDBVendor
 import net.liftweb.mapper.DB
-import net.liftweb.db.DefaultConnectionIdentifier
-import org.milmsearch.core.model.dao.DaoHelper
-import net.liftweb.common.Box
-import org.milmsearch.core.model.ComponentRegistry
-import org.milmsearch.core.model.service.MlProposalServiceComponent
+import net.liftweb.util.Props
 
 /**
  * サーブレットコンテナ(Tomcat等)の起動時に行う処理<br/>
  * 終了時に行う処理もここに書く
  */
-class Bootstrap extends ServletContextListener {
+class Bootstrap extends ServletContextListener with Loggable {
+
+  /**
+   * DB接続マネージャ
+   */
+  private var dbManager: Option[ProtoDBVendor] = None
 
   override def contextInitialized(event: ServletContextEvent) {
+    withErrlog { initializeDBCon() }
+  }
+
+  /**
+   * データベース接続を初期化する
+   */
+  private def initializeDBCon() {
+    val vendor = new StandardDBVendor(
+      Props.get("db.driver").get, // TODO default value
+      Props.get("db.url").get,    // TODO default value
+      Props.get("db.user"),
+      Props.get("db.password")
+    )
+
+    DB.defineConnectionManager(
+      DefaultConnectionIdentifier, vendor)
+
+    dbManager = Some(vendor)
   }
 
   override def contextDestroyed(finalizeevent: ServletContextEvent) {
+    withErrlogQuietly { closeDBCon() }
+  }
+
+  /**
+   * データベース接続を切断する
+   */
+  private def closeDBCon() {
+    dbManager foreach { _.closeAllConnections_!() }
   }
 
 }
